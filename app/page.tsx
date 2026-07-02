e5f5e96cc339549058acc54401958ef6a8bd8524
7parth <waradkarparth@gmail.com>
2026-07-02 23:00:50 +0530

    added loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 import PortfolioShell from "./components/PortfolioShell";
+import CinematicLoader from "./components/ui/CinematicLoader";
 
 export default function Home() {
-  return <PortfolioShell />;
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <>
+      <AnimatePresence>
+        {loading && <CinematicLoader onComplete={() => setLoading(false)} />}
+      </AnimatePresence>
+      {!loading && <PortfolioShell />}
+    </>
+  );
 }
